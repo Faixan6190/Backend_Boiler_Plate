8badc382cc0919ce4e67594d80167127a0baf11c
Faixan6190 <faixan6190@gmail.com>
2024-08-09 18:09:55 +0500

    add

--- src/modules/upload/routes.js
+++ src/modules/upload/routes.js
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import uploadController from "./controllers/upload.js";
 import multer from "multer";
+import path from "path";
 
+const __dirname = path.resolve();
 const routes = Router();
 
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "src/modules/upload/files");
+  },
+  filename: function (req, file, cb) {
+    const uniqueId = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, `${uniqueId}-${file.orginalname}`);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 routes.post("/", upload.single("file"), uploadController);
 
