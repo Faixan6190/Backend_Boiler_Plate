8bea2371be35553b8de43bc1797cb8cc809ad914
Faixan6190 <faixan6190@gmail.com>
2024-08-09 23:03:03 +0500

    fixing cloudinary

--- src/modules/upload/services/upload.js
+++ src/modules/upload/services/upload.js
@@ -1,6 +1,7 @@
 import { v2 as cloudinary } from "cloudinary";
 import { ENV } from "../../../constants/index.js";
 import fsExtra from "fs-extra";
+import sharp from "sharp";
 
 cloudinary.config({
   cloud_name: ENV.CLOUD_NAME,
@@ -10,22 +11,27 @@ cloudinary.config({
 
 const uploadFile = async (file) => {
   return new Promise((resolve, reject) => {
+    const dir = `src/modules/upload/files`;
     const filePath = `src/modules/upload/files/${file.filename}`;
-    cloudinary.uploader.upload(
-      filePath,
-      {
-        folder: "test-images",
-        public_id: file.filename,
-      },
-      function (error, result) {
-        if (error) {
-          reject(error);
-          return;
-        }
-        fsExtra.removeSync(filePath);
-        resolve(result);
-      }
-    );
+    sharp(filePath)
+      .jpeg({ quality: 40 })
+      .toFile(`${dir}/resize-${file.filename}`, async (err, info) => {
+        cloudinary.uploader.upload(
+          `${dir}/resize-${file.filename}`,
+          {
+            folder: "test-images",
+            public_id: file.filename,
+          },
+          function (error, result) {
+            if (error) {
+              reject(error);
+              return;
+            }
+            fsExtra.removeSync(filePath);
+            resolve(result);
+          }
+        );
+      });
   });
 };
 
