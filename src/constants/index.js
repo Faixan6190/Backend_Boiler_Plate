b661ecd70754bd1c32741c3488c17e94085d6624
Faixan6190 <faixan6190@gmail.com>
2024-08-23 22:07:29 +0500

    change

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,4 +7,5 @@ const UPDATE_DATA_MESSAGE = "Data updated successfully.";
 const INTERNAL_SERVER_ERROR_MESSAGE = "Internal server error.";
 const POST_DATA_MESSAGE = "Data added successfully.";
 
+
 export { ENV, DELETE_DATA_MESSAGE, UPDATE_DATA_MESSAGE, INTERNAL_SERVER_ERROR_MESSAGE, POST_DATA_MESSAGE };
